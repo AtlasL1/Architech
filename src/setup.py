b4066a806381642fa1053567e306e23790667551
Atlas 罗昕乐 <121162287+AtlasL1@users.noreply.github.com>
2023-12-04 18:29:38 +0800

    Create setup.py

---
+++ src/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='architech',
+    version='0.0.1',
+    author='Atlas',
+    description='A Python package for AI',
+    url='https://github.com/AtlasL1/Architech',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+)
