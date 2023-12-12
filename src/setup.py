9a9f0d2c0b5a17f5cca0012028139d52e9d69529
Atlas 罗昕乐 <loxingle1@gmail.com>
2023-12-12 21:32:20 +0800

    Update setup.py

--- src/setup.py
+++ src/setup.py
@@ -9,7 +9,7 @@ setup(
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
+        'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
