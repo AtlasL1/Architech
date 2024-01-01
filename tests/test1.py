266097157e2cc4aa0acf0f0135e7f2f087aceab3
Atlas 罗昕乐 <loxingle1@gmail.com>
2024-01-01 18:28:06 +0800

    Update test1.py

--- tests/test1.py
+++ tests/test1.py
@@ -1,2 +1,6 @@
-from architechture import start
-print(start.hello("a", "a3"))
+from architech import build
+print(build.name('Bob'))
+print(build.hello('a10'))
+print(build.hello_output('Le', 'hi'))
+print(build.recognition_add('Hello, how are you?', 'Hi! I\'m doing fine!'))
+print(build.dictionary_add('Hi', 'Hi!'))
