e07a88974562f294d347c79a8acacbfc864b3d1f
Atlas <loxingle1@gmail.com>
2024-01-12 17:21:52 +0800

    Update setup.py

--- src/setup.py
+++ src/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='architech',
-    version='0.1.1',
+    version='1.0.0',
     author='Atlas',
     description='A Python package for AI',
     url='https://github.com/AtlasL1/Architech',
